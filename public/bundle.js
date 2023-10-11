291793de7f17d02ac7c606e69a9cf619c800dfdf
Olasunmade Babafemi <babafemiolasunmade@gmail.com>
2023-10-11 10:34:52 +0100

    .

--- public/bundle.js
+++ public/bundle.js
@@ -1,4 +1,4 @@
-import * as axios from 'axios'
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm'; // Adjust the path accordingly
 
 
 export {axios}
--- server.js
+++ server.js
@@ -1,21 +1,90 @@
 
 import * as http from 'http'
-import * as url from 'url'
-const server = http.createServer((req, res) => {
+import * as fsB from 'fs'
+import { fileURLToPath } from 'url';
+import { dirname, join} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fs = fsB.promises
+const server = http.createServer(async (req, res) => {
   // Enable CORS for all routes
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Your other server logic here...
+ if (req.url === '/') {
+    const filePath = join(__dirname, 'public', 'index.html');
 
-  // For example, handle a simple GET request
-  if (req.method === 'GET' && req.url === '/index.js') {
-    // Respond with the contents of your index.js file
-    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-    res.end('console.log("Hello from index.js");');
-  } else {
-    // Handle other routes or methods
+    // Read the HTML file
+    try {
+      const data = await fs.readFile(filePath, 'utf8', (e, d) => {
+      // console.log("🚀 ~ file: server.js:27 ~ data ~ d:", d)
+        
+      });
+
+      // Respond with the HTML content
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    } catch (err) {
+      console.log("🚀 ~ file: server.js:30 ~ server ~ err:", err)
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  } else if (req.url === '/public/index.js') {
+    const jsFilePath = join(__dirname, 'public', 'index.js');
+
+    // Read the JavaScript file
+    try {
+      const jsData = await fs.readFile(jsFilePath, 'utf8', (e, d) =>{
+
+      });
+
+      // Respond with the JavaScript content
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      res.end(jsData);
+    } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+
+  } else if (req.url === '/public/bundle.js') {
+    const jsFilePath = join(__dirname, 'public', 'bundle.js');
+
+    // Read the JavaScript file
+    try {
+      const jsData = await fs.readFile(jsFilePath, 'utf8', (e, d) =>{
+        
+      });
+
+      // Respond with the JavaScript content
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      res.end(jsData);
+    } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+
+  } else if (req.url === '/node_modules/axios/index.js ') {
+    const jsFilePath = join(__dirname, 'node_modules', 'axios', 'index.js');
+
+    // Read the JavaScript file
+    try {
+      const jsData = await fs.readFile(jsFilePath, 'utf8', (e, d) =>{
+        
+      });
+
+      // Respond with the JavaScript content
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      res.end(jsData);
+    } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+
+  }else {
+    // Handle other routes
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
